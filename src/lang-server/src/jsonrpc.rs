dc0992d28c20c376a1aadf5865eeb8ffe3a4a9d0
Domantas Jadenkus <djadenkus@gmail.com>
2017-09-15 13:21:07 +0300

    basic language server that reports syntax errors

---
+++ src/lang-server/src/jsonrpc.rs
@@ -0,0 +1,294 @@
+use std::collections::HashMap;
+use languageserver_types as lst;
+use serde::Serialize;
+use serde::de::DeserializeOwned;
+use serde_json::{self, Value as JsonValue};
+
+
+#[derive(Deserialize)]
+struct Request {
+    jsonrpc: String,
+    id: Option<lst::NumberOrString>,
+    method: String,
+    params: JsonValue,
+}
+
+#[derive(Serialize)]
+pub struct ErrorResponse<T> {
+    code: i64,
+    message: String,
+    #[serde(skip_serializing_if="Option::is_none")]
+    data: Option<T>,
+}
+
+impl<T> ErrorResponse<T> {
+    fn parse_error() -> Self {
+        ErrorResponse {
+            code: -32700,
+            message: "Parse error".into(),
+            data: None,
+        }
+    }
+
+    fn invalid_request() -> Self {
+        ErrorResponse {
+            code: -32600,
+            message: "Invalid request".into(),
+            data: None,
+        }
+    }
+
+    fn method_not_found() -> Self {
+        ErrorResponse {
+            code: -32601,
+            message: "Method not found".into(),
+            data: None,
+        }
+    }
+
+    fn invalid_params() -> Self {
+        ErrorResponse {
+            code: -32602,
+            message: "Invalid params".into(),
+            data: None,
+        }
+    }
+
+    fn internal_error() -> Self {
+        ErrorResponse {
+            code: -32603,
+            message: "Internal error".into(),
+            data: None,
+        }
+    }
+
+    pub fn new<S: Into<String>>(code: i64, msg: S, data: Option<T>) -> Self {
+        ErrorResponse {
+            code,
+            message: msg.into(),
+            data,
+        }
+    }
+}
+
+impl<T: Serialize> ErrorResponse<T> {
+    fn serialize_error(self) -> ErrorResponse<JsonValue> {
+        let ErrorResponse { code, message, data } = self;
+        let data = if let Some(value) = data {
+            match serde_json::to_value(&value) {
+                Ok(value) => Some(value),
+                Err(_) => {
+                    // failed to serialize error value,
+                    // just respond with internal JSON-RPC error
+                    return ErrorResponse::internal_error();
+                }
+            }
+        } else {
+            None
+        };
+        ErrorResponse {
+            code,
+            message,
+            data,
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct RawResponse<T, E> {
+    jsonrpc: &'static str,
+    #[serde(skip_serializing_if="Option::is_none")]
+    result: Option<T>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    error: Option<ErrorResponse<E>>,
+    id: Option<lst::NumberOrString>,
+}
+
+impl<T: Serialize, E: Serialize> RawResponse<T, E> {
+    fn ok(result: T, id: Option<lst::NumberOrString>) -> Self {
+        RawResponse {
+            jsonrpc: "2.0",
+            result: Some(result),
+            error: None,
+            id,
+        }
+    }
+
+    fn err(error: ErrorResponse<E>, id: Option<lst::NumberOrString>) -> Self {
+        RawResponse {
+            jsonrpc: "2.0",
+            result: None,
+            error: Some(error),
+            id,
+        }
+    }
+
+    fn serialize(&self) -> JsonValue {
+        serde_json::to_value(&self).unwrap_or_else(|_| {
+            let err = RawResponse::<(), ()>::err(ErrorResponse::internal_error(), None);
+            serde_json::to_value(&err)
+                .expect("failed to serialize basic response")
+        })
+    }
+}
+
+pub enum Response<T, E> {
+    Success(T),
+    Error(ErrorResponse<E>),
+}
+
+impl<T, E> Response<T, E> {
+    pub fn map<U, F: FnOnce(T) -> U>(self, f: F) -> Response<U, E> {
+        match self {
+            Response::Success(value) => Response::Success(f(value)),
+            Response::Error(err) => Response::Error(err),
+        }
+    }
+}
+
+impl<T: Serialize, E: Serialize> Response<T, E> {
+    fn to_raw(self, id: Option<lst::NumberOrString>) -> RawResponse<T, E> {
+        match self {
+            Response::Success(value) => {
+                RawResponse::ok(value, id)
+            }
+            Response::Error(err) => {
+                RawResponse::err(err, id)
+            }
+        }
+    }
+}
+
+type CallHandler<'a, T, E> = Box<Fn(JsonValue) -> Option<Response<T, E>> + 'a>;
+
+#[derive(Default)]
+pub struct RpcHandler<'a> {
+    handlers: HashMap<String, CallHandler<'a, JsonValue, JsonValue>>,
+}
+
+impl<'a> RpcHandler<'a> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn add_method<S, F, T, U, E>(&mut self, name: S, handle: F)
+        where
+            F: Fn(T) -> Response<U, E> + 'a,
+            T: DeserializeOwned,
+            U: Serialize,
+            E: Serialize,
+            S: Into<String>
+    {
+        self.add_handler(name, move |input| {
+            handle(input).map(Some)
+        });
+    }
+
+    pub fn add_notification<S, F, T>(&mut self, name: S, handle: F)
+        where
+            F: Fn(T) + 'a,
+            T: DeserializeOwned,
+            S: Into<String>
+    {
+        self.add_handler::<_, _, _, (), ()>(name, move |input| {
+            handle(input);
+            Response::Success(None)
+        });
+    }
+
+    fn add_handler<S, F, T, U, E>(&mut self, name: S, handle: F)
+        where
+            F: Fn(T) -> Response<Option<U>, E> + 'a,
+            T: DeserializeOwned,
+            U: Serialize,
+            E: Serialize,
+            S: Into<String>
+    {
+        let handle = move |params| {
+            let params = match serde_json::from_value::<T>(params) {
+                Ok(params) => params,
+                Err(_) => {
+                    let response = ErrorResponse::invalid_params();
+                    return Some(Response::Error(response));
+                }
+            };
+            match handle(params) {
+                Response::Success(Some(response)) => {
+                    let json = serde_json::to_value(&response).unwrap();
+                    Some(Response::Success(json))
+                }
+                Response::Success(None) => {
+                    None
+                }
+                Response::Error(err) => {
+                    Some(Response::Error(err.serialize_error()))
+                }
+            }
+        };
+        self.handlers.insert(name.into(), Box::new(handle));
+    }
+
+    pub fn handle_call(&mut self, input: &str) -> Option<String> {
+        let value = match serde_json::from_str::<JsonValue>(input) {
+            Ok(value) => value,
+            Err(_) => return basic_error(ErrorResponse::parse_error()),
+        };
+
+        let response = match value {
+            JsonValue::Array(values) => {
+                // batch request
+                if values.len() == 0 {
+                    return basic_error(ErrorResponse::invalid_request());
+                }
+                let mut responses = Vec::new();
+                for request in values {
+                    if let Some(response) = self.handle_request(request) {
+                        responses.push(response.serialize());
+                    }
+                }
+                if responses.is_empty() {
+                    None
+                } else {
+                    Some(JsonValue::Array(responses))
+                }
+            }
+            value => {
+                // single request
+                self.handle_request(value).map(|r| r.serialize())
+            }
+        };
+        response.map(|value| serde_json::to_string(&value)
+            .expect("failed to serialize json value to string"))
+    }
+
+    fn handle_request(&mut self, request: JsonValue) -> Option<RawResponse<JsonValue, JsonValue>> {
+        let request = match serde_json::from_value::<Request>(request) {
+            Ok(request) => request,
+            Err(_) => return Some(RawResponse::err(ErrorResponse::invalid_request(), None)),
+        };
+        if request.id.is_some() {
+            debug!("handling method: '{}'", request.method);
+        } else {
+            debug!("handling notification: '{}'", request.method);
+        }
+        match self.handlers.get_mut(&request.method) {
+            Some(handler) => {
+                let id = request.id;
+                handler(request.params).map(|resp| resp.to_raw(id))
+            }
+            None => {
+                error!("handler not found for: '{}'", request.method);
+                // only reply to requests, not notifications
+                if request.id.is_some() {
+                    Some(RawResponse::err(ErrorResponse::method_not_found(), request.id))
+                } else {
+                    None
+                }
+            }
+        }
+    }
+}
+
+fn basic_error(err: ErrorResponse<()>) -> Option<String> {
+    Some(serde_json::to_string(&err).expect("failed to serialize basic error"))
+}
---
+++ src/lang-server/src/main.rs
@@ -0,0 +1,157 @@
+#![allow(dead_code)]
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+extern crate languageserver_types;
+#[macro_use]
+extern crate log;
+extern crate simple_logging;
+extern crate url;
+extern crate syntax;
+extern crate errors;
+
+// mod protocol;
+// mod server;
+
+mod transport;
+mod jsonrpc;
+
+use std::cell::RefCell;
+use std::io;
+use std::io::{BufRead, Write};
+use std::ops::DerefMut;
+use languageserver_types as lst;
+use url::Url;
+use jsonrpc::{RpcHandler, Response};
+use transport::Transport;
+
+
+fn main() {
+    simple_logging::log_to_stderr(log::LogLevelFilter::Trace)
+        .expect("failed to initialize logging");
+    
+    let stdin = io::stdin();
+    let stdin = stdin.lock();
+    let stdout = io::stdout();
+    let stdout = stdout.lock();
+
+    let transport = RefCell::new(Transport::new(stdin, stdout));
+    let mut rpc = RpcHandler::new();
+
+    rpc.add_method("initialize", |_: lst::InitializeParams| {
+        Response::Success::<_, ()>(lst::InitializeResult {
+            capabilities: lst::ServerCapabilities {
+                text_document_sync: Some(lst::TextDocumentSyncKind::Full),
+                .. Default::default()
+            },
+        })
+    });
+
+    rpc.add_notification("initialized", |_: serde_json::Value| {
+    });
+
+    rpc.add_notification("textDocument/didOpen", |params: lst::DidOpenTextDocumentParams| {
+        publish_diagnostics(
+            &params.text_document.text,
+            params.text_document.uri,
+            transport.borrow_mut().deref_mut(),
+        );
+    });
+
+    rpc.add_notification("textDocument/didSave", |_: lst::DidSaveTextDocumentParams| {
+    });
+
+    rpc.add_notification("textDocument/didClose", |_: lst::DidCloseTextDocumentParams| {
+    });
+
+    rpc.add_notification("textDocument/didChange", |params: lst::DidChangeTextDocumentParams| {
+        if params.content_changes.len() != 1 ||
+            params.content_changes[0].range.is_some() ||
+            params.content_changes[0].range_length.is_some()
+        {
+            debug!("got bad edit with `didChange` event");
+        } else {
+            publish_diagnostics(
+                &params.content_changes[0].text,
+                params.text_document.uri,
+                transport.borrow_mut().deref_mut(),
+            );
+        }
+    });
+
+    loop {
+        let response = {
+            let msg = transport
+                .borrow_mut()
+                .read_message()
+                .expect("failed to read message");
+            rpc.handle_call(&msg)
+        };
+        if let Some(response) = response {
+            transport
+                .borrow_mut()
+                .send_message(&response)
+                .expect("failed to send");
+        }
+    }
+}
+
+fn publish_diagnostics<R, W>(source: &str, doc: Url, transport: &mut Transport<R, W>)
+    where R: BufRead, W: Write
+{
+    let diagnostics = make_diagnostics(source);
+    let params = lst::PublishDiagnosticsParams {
+        uri: doc,
+        diagnostics,
+    };
+    #[derive(Serialize)]
+    struct Notification {
+        jsonrpc: &'static str,
+        method: &'static str,
+        params: lst::PublishDiagnosticsParams,
+    }
+
+    let notification = Notification {
+        jsonrpc: "2.0",
+        method: "textDocument/publishDiagnostics",
+        params,
+    };
+    let string = serde_json::to_string(&notification)
+        .expect("failed to serialize diagnostics");
+    transport.send_message(&string).expect("failed to write message");
+}
+
+fn make_diagnostics(source: &str) -> Vec<lst::Diagnostic> {
+    let reporter = errors::Reporter::new();
+    let tokens = syntax::lex(source, reporter.clone());
+    let _ = syntax::parse(tokens, reporter.clone());
+    reporter
+        .get_diagnostics()
+        .into_iter()
+        .map(|d| note_to_diagnostic(&d.notes[0], d.message))
+        .collect()
+}
+
+fn note_to_diagnostic(note: &errors::reporter::Note, msg: String) -> lst::Diagnostic {
+    fn convert_pos(pos: errors::position::Position) -> lst::Position {
+        lst::Position {
+            line: (pos.line - 1) as u64,
+            character: (pos.column - 1) as u64,
+        }
+    }
+    fn convert_range(range: errors::position::Span) -> lst::Range {
+        lst::Range {
+            start: convert_pos(range.start),
+            end: convert_pos(range.end),
+        }
+    }
+    lst::Diagnostic {
+        range: convert_range(note.span),
+        severity: Some(lst::DiagnosticSeverity::Error),
+        code: None,
+        source: Some("plank".into()),
+        message: msg,
+    }
+}
