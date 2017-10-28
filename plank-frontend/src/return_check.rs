55f159ac68724f89cfb8dfd5eaf2e44bce60d1ec
Domantas Jadenkus <djadenkus@gmail.com>
2017-10-28 14:18:39 +0300

    account for parse errors in return check

--- plank-frontend/src/return_check.rs
+++ plank-frontend/src/return_check.rs
@@ -1,21 +1,52 @@
-use ast::cfg::{BlockEnd, Function, Program};
+use std::collections::{HashSet, VecDeque};
+use ast::cfg::{Block, BlockEnd, Function, Instruction, Program};
 use CompileCtx;
 
 
-fn check_function(f: &Function, ctx: &mut CompileCtx) {
-    fn is_error(end: &BlockEnd) -> bool {
-        match *end {
-            BlockEnd::Error => true,
-            _ => false,
+fn has_error_statement(block: &Block) -> bool {
+    for op in &block.ops {
+        match **op {
+            Instruction::Error => return true,
+            _ => {}
         }
     }
+    false
+}
 
-    if f.blocks.iter().any(|(_, b)| is_error(&b.end)) {
-        let span = f.complete_span;
-        ctx.reporter
-            .error("not all paths return a value", span)
-            .span(span)
-            .build();
+fn check_function(f: &Function, ctx: &mut CompileCtx) {
+    let mut queue = VecDeque::new();
+    let mut visited = HashSet::new();
+    if let Some(start_block) = f.start_block {
+        queue.push_back(start_block);
+    }
+
+    while let Some(block) = queue.pop_front() {
+        if visited.contains(&block) {
+            continue;
+        }
+        visited.insert(block);
+        let block = &f.blocks[&block];
+        if has_error_statement(block) {
+            continue;
+        }
+        match block.end {
+            BlockEnd::Error => {
+                let span = f.complete_span;
+                ctx.reporter
+                    .error("not all paths return a value", span)
+                    .span(span)
+                    .build();
+                return;
+            }
+            BlockEnd::Branch(_, a, b) => {
+                queue.push_back(a);
+                queue.push_back(b);
+            }
+            BlockEnd::Jump(a) => {
+                queue.push_back(a);
+            }
+            BlockEnd::Return(_) => {}
+        }
     }
 }
 
